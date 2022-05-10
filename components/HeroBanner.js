af3487b3a02e7c7e8237d40a035240f6493db4d5
Rumeysa <rumeysayuk90@gmail.com>
2022-05-10 21:05:10 +0300

    .

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 import {urlFor} from "../lib/client";
 
 const HeroBanner = ({heroBanner}) => {
-   console.log(heroBanner);
    return (
       <div className="hero-banner-container">
          <div>
             <p className="beats-solo">{heroBanner.smallText}</p>
             <h3>{heroBanner.midText}</h3>
+            <h1>{heroBanner.largeText1}</h1>
             <img src={urlFor(heroBanner.image)} alt="tshirt" className="hero-banner-image"/>
             <div>
                <Link href={`/product/${heroBanner.product}`}>
--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Head from 'next/head';
+import {Footer, Navbar} from "./index";
 
-const Layout = () => {
+const Layout = ({children}) => {
    return (
-      <div>
-hello
+      <div className="layout">
+         <Head>
+            <title>Rumeysa Store</title>
+         </Head>
+         <header>
+            <Navbar/>
+         </header>
+         <main className="main-container">
+            {children}
+         </main>
+         <footer>
+            <Footer/>
+         </footer>
       </div>
    )
 }
