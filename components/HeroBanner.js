7d67bb6c6b475f60f017264f51f56c4744ebc029
Rumeysa <rumeysayuk90@gmail.com>
2022-05-10 20:02:52 +0300

    .

---
+++ components/HeroBanner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from "next/link";
+import {urlFor} from "../lib/client";
+
+const HeroBanner = ({heroBanner}) => {
+   console.log(heroBanner);
+   return (
+      <div className="hero-banner-container">
+         <div>
+            <p className="beats-solo">{heroBanner.smallText}</p>
+            <h3>{heroBanner.midText}</h3>
+            <img src={urlFor(heroBanner.image)} alt="tshirt" className="hero-banner-image"/>
+            <div>
+               <Link href={`/product/${heroBanner.product}`}>
+                  <button type="button">{heroBanner.buttonText}</button>
+               </Link>
+               <div className="desc">
+                  <h5>Description</h5>
+                  <p>{heroBanner.description}</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default HeroBanner;
